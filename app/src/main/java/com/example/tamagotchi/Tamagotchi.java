13a4ff27f323b82cbaecc5feaea6c97166e5e090
justin <justin_5529q@protonmail.com>
2022-01-25 16:41:34 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/tamagotchi/Tamagotchi.java
@@ -0,0 +1,14 @@
+package com.example.tamagotchi;
+
+public class Tamagotchi {
+
+    int health;
+    int hunger;
+
+    public static void main(String[] args) {
+
+    }
+    public timer(){
+        Tstart = SystemClock.
+    }
+}
